324773d1a37858ccf725f5143d439a1b4b92029e
Nathan Weber <nweber@digitalprimates.net>
2015-11-05 09:04:02 -0600

    Attempt to expose values for use with Browserify.

---
+++ src/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+    TimelineLite: require('./uncompressed/TimelineLite'),
+    TimelineMax: require('./uncompressed/TimelineMax'),
+    TweenLite: require('./uncompressed/TweenLite'),
+    TweenMax: require('./uncompressed/TweenMax'),
+    Draggable: require('./uncompressed/utils/Draggable'),
+    Easing: require('./uncompressed/easing/EasePack')
+};
